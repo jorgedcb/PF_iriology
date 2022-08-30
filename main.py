82057011da2acf565ea1602abcccaf2f79f238e7
Jorge Eduardo Castilla Bonet <jecb2001@gmail.com>
2022-08-29 23:34:53 -0500

    version 1 gui

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
-import numpy as np
-import cv2
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+import tkinter as tk                    
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+from PIL import Image, ImageTk
+from iris_detection import Iris_detection
 
-class iris_detection():
-    def __init__(self, image_path):
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self._iris = None
         self._img = None
-        self._img_path = image_path
-        self._pupil = None
-
-    def load_image(self):
-
-        self._img = cv2.imread(self._img_path)
-        if type(self._img) is type(None):
-            return False
-        else:
-            return True
+        self.title("Tab Widget")
+        tabControl = ttk.Notebook(self)
         
-    def convert_to_gray_scale(self):
-        self._img = cv2.cvtColor(self._img, cv2.COLOR_BGR2GRAY)
+        tab1 = ttk.Frame(tabControl)
+        self.tab1 = tab1
+        tab2 = ttk.Frame(tabControl)
+        self.tab2 = tab2
+        tabControl.add(tab1, text ='Carga')
+        tabControl.add(tab2, text ='Resultados')
+        tabControl.pack(expand = 1, fill ="both")
 
-    def detect_pupil(self):
-        _, thresh = cv2.threshold(self._img, 100, 255, cv2.THRESH_BINARY)
-        img_with_contours = np.copy(self._img)
-        c = cv2.HoughCircles(img_with_contours, cv2.HOUGH_GRADIENT, 2, self._img.shape[0] / 2, maxRadius=60)
-        for l in c:
-            for circle in l:
-                center = (int(circle[0]), int(circle[1]))
-                radius = int(circle[2])
-                cv2.circle(self._img, center, radius, (0, 0, 0), thickness=-1)
-                self._pupil = (center[0], center[1], radius)
+        my_font1=('times', 18, 'bold')
+        l1 = tk.Label(tab1,text='Seleccione imagen para empezar',width=30,font=my_font1)  
+        l1.grid(row=1,column=1)
+        b1 = tk.Button(tab1, text='Cargar foto', 
+        width=20,command = lambda:self.upload_file())
+        b1.grid(row=2,column=1) 
 
-    def detect_iris(self):
-        _, t = cv2.threshold(self._img, 195, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(t, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        my_font1=('times', 18, 'bold')
+        l1 = tk.Label(tab2,text='Seleccione imagen para empezar',width=30,font=my_font1)  
+        l1.grid(row=1,column=1)
 
-        img_with_contours = np.copy(self._img)
-        cv2.drawContours(img_with_contours, contours, -1, (0, 255, 0))
-        c = cv2.HoughCircles(img_with_contours, cv2.HOUGH_GRADIENT, 2, self._pupil[2] * 2, maxRadius=170, minRadius=80)
-        for l in c:
-            for circle in l:
-                center = (self._pupil[0], self._pupil[1])
-                radius = int(circle[2])
-                mask = np.zeros((self._img.shape[0], self._img.shape[1], 1), np.uint8)
-                cv2.circle(mask, center, radius, (255, 255, 255), thickness = -1)
-                self._img = cv2.bitwise_and(self._img, mask)
+    def upload_file(self):
+        f_types = [('Jpg Files', '*.jpg')]
+        filename = filedialog.askopenfilename(filetypes=f_types)
+        print(filename)
+        img = ImageTk.PhotoImage(file=filename)
+        self._img = img
+        b2 =tk.Button(self.tab1,image=img) # using Button 
+        b2.grid(row=3,column=1)
+        self._iris = Iris_detection(filename)
+        img = self._iris.start_detection()
+        im = Image.fromarray(img)
+        imgtk = ImageTk.PhotoImage(image=im)
+        b2 =tk.Button(self.tab2,image= self._img) # using Button 
+        b2.grid(row=2,column=1)
+        b3 =tk.Button(self.tab2,image= self._img) # using Button 
+        b3.grid(row=3,column=1)
+        b4 =tk.Button(self.tab2,image=imgtk) # using Button 
+        b4.grid(row=2,column=2)
+        self.mainloop()  
 
-    def start_detection(self):
-  
-        if(self.load_image()):
-            self.convert_to_gray_scale()
-            self.detect_pupil()
-            self.detect_iris()
-            cv2.imshow("Result", self._img)
-            cv2.waitKey(0)
-        else:
-            print('Image file "' + self._img_path + '" could not be loaded.')
-        
-id = iris_detection('095_1_1.jpg')
-id.start_detection()
+
+    def button_clicked(self):
+        showinfo(title='Information', message='Hello, Tkinter!')
+
+if __name__ == "__main__":
+  app = App()
+  app.mainloop()
